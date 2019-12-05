8590ee5c3fe5411fbe2d397b850536ba95086c59
GodzillaJim <jimnanjoroge76@gmail.com>
2019-12-05 23:09:36 +0300

    3 files modified

--- Hello.js
+++ Hello.js
@@ -5,13 +5,22 @@ import {Row, Col} from 'react-bootstrap';
 import {Form} from 'react-bootstrap';
 import './style.css';
 
+
 class Body extends React.Component{
   constructor(props){
     super(props);
+    let init = "A man who says I am king is not true king.";
     this.state = {
-      originalText : ""
-    }
+      originalText : init,
+      finalText:""
+    };
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
+ handleUpdate = () => {
+   let temp = document.getElementById("editor").value;
+   temp = marked(temp);
+   this.setState({finalText: temp});
+ }
   
   render(){
     return (
@@ -20,11 +29,11 @@ class Body extends React.Component{
           <div className="row form-class">
             <div className="col contain">
             <label for="editor" className=" form-label">Editor:</label>
-            <textarea className=" form-control" id="editor"></textarea>
+            <textarea className=" form-control" id="editor" value={this.state.originalText} onChange={this.handleUpdate}></textarea>
           </div>
            <div className="col  contain">
             <label for="editor" className="form-label">Preview:</label>
-            <textarea style={{backgroundColor:" darkslategrey"}}className="form-control"></textarea>
+            <textarea value={this.state.initMarked} style={{backgroundColor:" darkslategrey",color:"red"}}className="form-control"></textarea>
           </div>
         </div>
         </div>
